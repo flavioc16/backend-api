94546dc4b89d31af116f1fd8808a1b3750b80234
Flavio Castro <flavio2008sousa@hotmail.com.com>
2025-01-01 11:05:04 -0300

    projeto para deploy

--- dist/src/jobs/backups/backup-sql-2024-12-31.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO user (id, name, username, password, role, created_at, updated_at) VALUES
-('da09a3a6-2439-4ee9-ae63-676929660ef3', 'admin', 'admin', '$2a$10$KHiDumHpX93R/RC.tpa2wOw2tY6TlQ9P64ZlZx9otOyEvyP/3zamW', 'ADMIN', '2024-07-23 13:00:57', '2024-07-23 13:00:57');
-
