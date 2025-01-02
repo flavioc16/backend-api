ad8e3ad6b2c39ce5820c558bfcd6e1b53b49c464
Flavio Castro <flavio2008sousa@hotmail.com.com>
2025-01-02 17:36:37 -0300

    deploy teste juros

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import { commitAutomatico } from './jobs/autoCommit';
 import { criarBackupSQL } from './jobs/criarBackupSQL';
 
 aplicarJuros();
-criarBackupSQL();
+//criarBackupSQL();
 //commitAutomatico();
 
 // Função de juros
@@ -20,28 +20,28 @@ cron.schedule('0 0 * * *', async () => {
 });
 
 // Função de backup SQL
-cron.schedule('50 11 * * *', async () => {
-  console.log('Executando backup completo...');
-  await criarBackupSQL();
-});
+// cron.schedule('50 11 * * *', async () => {
+//   console.log('Executando backup completo...');
+//   await criarBackupSQL();
+// });
 
 
 // Função de commit (agendado para todos os dias, por exemplo)
-cron.schedule('55 11 * * *', async () => {
-  console.log('Chamando a função de commit');
-  await commitAutomatico(); // Chama a função de commit
-});
+// cron.schedule('55 11 * * *', async () => {
+//   console.log('Chamando a função de commit');
+//   await commitAutomatico(); // Chama a função de commit
+// });
 
-cron.schedule('0 16 * * *', async () => {
-  console.log('Executando backup completo...');
-  await criarBackupSQL();
-});
+// cron.schedule('0 16 * * *', async () => {
+//   console.log('Executando backup completo...');
+//   await criarBackupSQL();
+// });
 
 // Função de commit (agendado para todos os dias, por exemplo)
-cron.schedule('5 16 * * *', async () => {  // Agendado para 3:00 AM todos os dias
-  console.log('Chamando a função de commit');
-  await commitAutomatico(); // Chama a função de commit
-});
+// cron.schedule('5 16 * * *', async () => {  // Agendado para 3:00 AM todos os dias
+//   console.log('Chamando a função de commit');
+//   await commitAutomatico(); // Chama a função de commit
+// });
 
 // cron.schedule('* * * * *', async () => {
 //   console.log('Executando backup completo...');
